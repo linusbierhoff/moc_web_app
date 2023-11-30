a5f4298c72f3e5dc47c7b60275f69199b798da68
Linus Bierhoff <linus.bierhoff@icloud.com>
2023-11-30 19:42:17 +0100

    init

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# moc_web_app
+# mobile_computing_web_app
+
+ This is a quizapp, where the user can answer the questions by tilting his phone in one of the four directions.
---
+++ script.js
@@ -0,0 +1,27 @@
+var orientationDisplay = document.getElementById('orientation-display');
+orientationDisplay.innerHTML = `<p>Loading...</p>`;
+
+
+if (window.DeviceMotionEvent) {
+    window.addEventListener('deviceorientation', handleOrientation);
+} else {
+    var orientationDisplay = document.getElementById('orientation-display');
+    orientationDisplay.innerHTML = `<p>Device orientation not supported</p>`;
+}
+
+
+function handleOrientation(event) {
+    var alpha = event.alpha;
+    var beta = event.beta;  
+    var gamma = event.gamma; 
+    updateOrientationDisplay(alpha, beta, gamma);
+}
+
+function updateOrientationDisplay(alpha, beta, gamma) {
+    var orientationDisplay = document.getElementById('orientation-display');
+    orientationDisplay.innerHTML = `
+        <p>Alpha (Z-axis): ${alpha.toFixed(2)} degrees</p>
+        <p>Beta (X-axis): ${beta.toFixed(2)} degrees</p>
+        <p>Gamma (Y-axis): ${gamma.toFixed(2)} degrees</p>
+    `;
+}
