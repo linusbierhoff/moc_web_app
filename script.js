aae0b0b07b7701e044a92b88f3582866c5ca08aa
Linus Bierhoff <linus.bierhoff@icloud.com>
2023-11-30 20:28:06 +0100

    Change

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ var initAlpha = null
 var initBeta = null
 var initGamma = null
 
+var enabled = false
+
+function onButton(){
+    if(enabled) {
+        resetOrientation();
+    } else{
+        requestOrientationPermission();
+    }
+}
 
 
 function requestOrientationPermission(){
@@ -24,7 +33,18 @@ function handleOrientation(event) {
     initAlpha ??= alpha;
     initBeta ??= beta;
     initGamma ??= gamma;
+    getPhoneSide(alpha- initAlpha, beta - initBeta, gamma - initGamma)
     updateOrientationDisplay(alpha - initAlpha, beta - initBeta, gamma - initGamma);
+}
+
+function resetOrientation() {
+    var alpha = DeviceOrientationEvent.alpha;
+    var beta = DeviceOrientationEvent.beta;  
+    var gamma = DeviceOrientationEvent.gamma; 
+
+    initAlpha = alpha;
+    initBeta = beta;
+    initGamma = gamma;
 }
 
 function updateOrientationDisplay(alpha, beta, gamma) {
@@ -38,5 +58,10 @@ function updateOrientationDisplay(alpha, beta, gamma) {
 
 
 function getPhoneSide(alpha, beta, gamma) {
-    var orientationDisplay = document.getElementById('orientation-display');
+    var orientationDisplay = document.getElementById('side-display');
+    if(beta < -30) orientationDisplay.innerHTML = `<h2>Oben</h2>`
+    if(beta > 30) orientationDisplay.innerHTML = `<h2>Unten</h2>`
+
+    if(gamma < -30) orientationDisplay.innerHTML = `<h2>Links</h2>`
+    if(hamma > 30) orientationDisplay.innerHTML = `<h2>Rechts</h2>`
 }
