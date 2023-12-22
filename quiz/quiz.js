b96a084af1bf05a06b2162349aea4840c67407fa
Linus Bierhoff <linus.bierhoff@icloud.com>
2023-12-22 11:15:01 +0100

    Auto commit by script on 2023-12-22 11:15:01

---
+++ quiz/quiz.js
@@ -0,0 +1,61 @@
+let initAlpha = null;
+let initBeta = null;
+let initGamma = null;
+
+let enabled = false;
+
+const angle = 30;
+
+window.addEventListener('deviceorientation', handleOrientation)
+
+function resetOrientation() {
+    initAlpha = initBeta = initGamma = null;
+}
+
+
+function handleOrientation(event) {
+    let alpha = event.alpha;
+    let beta = event.beta;
+    let gamma = event.gamma;
+
+    initAlpha ??= alpha;
+    initBeta ??= beta;
+    initGamma ??= gamma;
+
+    alpha = alpha - initAlpha
+    beta = beta - initBeta
+    gamma = gamma - initGamma
+
+    updatePhoneSide(alpha, beta, gamma)
+}
+
+function updatePhoneSide(alpha, beta, gamma) {
+    const sideDisplay = document.getElementById('side-display');
+
+    const absBeta = Math.abs(beta);
+    const absGamma = Math.abs(gamma);
+
+    if (absBeta > absGamma && absBeta > angle) {
+        if (beta < -angle) showAnswerById(1);
+        if (beta > angle) showAnswerById(4);
+        return;
+    }
+    if (absGamma > absBeta && absGamma > angle) {
+        if (gamma < -angle) showAnswerById(2);
+        if (gamma > angle) showAnswerById(3);
+        return;
+    }
+
+    showAnswerById(0)
+}
+
+function showAnswerById(id) {
+    const answerBox = document.getElementById(`answer-${id}`)
+    if (id === 1 || id === 4) {
+        answerBox.innerHeight(40)
+    }
+
+    if (id === 2 || id === 3) {
+        answerBox.innerWidth(40)
+    }
+}
--- script.js
+++ script.js
@@ -1,84 +1,13 @@
-let initAlpha = null;
-let initBeta = null;
-let initGamma = null;
-
-let enabled = false;
-
-const angle = 30;
-
-
-document.getElementById('permission-button').addEventListener('click', onButton)
-
-
-function onButton() {
-    if (!enabled) {
-        requestOrientationPermission();
-        enabled = true
-    } else {
-        resetOrientation();
-    }
-}
+document.getElementById('start-button').addEventListener('click', requestOrientationPermission)
 
 
 function requestOrientationPermission() {
     DeviceOrientationEvent.requestPermission()
         .then(response => {
             if (response === 'granted') {
+                window.location.href = "quiz/quiz.html";
                 window.addEventListener('deviceorientation', handleOrientation)
             }
         })
         .catch(console.error)
-}
-
-
-function resetOrientation() {
-    initAlpha = initBeta = initGamma = null;
-}
-
-
-function handleOrientation(event) {
-    let alpha = event.alpha;
-    let beta = event.beta;
-    let gamma = event.gamma;
-
-    initAlpha ??= alpha;
-    initBeta ??= beta;
-    initGamma ??= gamma;
-
-    alpha = alpha - initAlpha
-    beta = beta - initBeta
-    gamma = gamma - initGamma
-
-    updatePhoneSide(alpha, beta, gamma)
-    updateOrientationDisplay(alpha, beta, gamma);
-}
-
-
-function updateOrientationDisplay(alpha, beta, gamma) {
-    const orientationDisplay = document.getElementById('orientation-display');
-    orientationDisplay.innerHTML = `
-        <p>Alpha (Z-axis): ${alpha.toFixed(2)} degrees</p>
-        <p>Beta (X-axis): ${beta.toFixed(2)} degrees</p>
-        <p>Gamma (Y-axis): ${gamma.toFixed(2)} degrees</p>
-    `;
-}
-
-
-function updatePhoneSide(alpha, beta, gamma) {
-    const sideDisplay = document.getElementById('side-display');
-
-    const absBeta = Math.abs(beta);
-    const absGamma = Math.abs(gamma);
-
-    if (absBeta > absGamma && absBeta > angle) {
-        if (beta < -angle) sideDisplay.innerHTML = `<h4>Up</h4>`;
-        if (beta > angle) sideDisplay.innerHTML = `<h4>Down</h4>`;
-        return;
-    }
-    if (absGamma > absBeta && absGamma > angle) {
-        if (gamma < -angle) sideDisplay.innerHTML = `<h4>Left</h4>`;
-        if (gamma > angle) sideDisplay.innerHTML = `<h4>Right</h4>`;
-        return;
-    }
-    sideDisplay.innerHTML = ``;
 }
