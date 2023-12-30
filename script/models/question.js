2f7aef66506887aec169275304199d4272307c40
Linus Bierhoff <linus.bierhoff@icloud.com>
2023-12-30 16:07:01 +0100

    Auto commit by script on 2023-12-30 16:07:01

--- script/models/question.js
+++ script/models/question.js
@@ -1,9 +1,6 @@
 function Question(question, wrong_answers, right_answer) {
     this.question = question
-    this.answers = []
-    this.answers.push(wrong_answers)
-    this.answers.push(right_answer)
-    this.answers = this.answers.sort((a, b) => 0.5 - Math.random());
+    this.answers = [wrong_answers, right_answer].sort(() => Math.random() - 0.5);
 
     this.correct_answer = this.answers.indexOf(right_answer)
 }
